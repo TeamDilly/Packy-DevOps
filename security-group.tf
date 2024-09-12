a1661d8fbf57cf0660aa011e24c07bee2fc7c2d0
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-09-12 14:35:50 +0900

    fix: 웹용 EC2 Security Group 분리

--- security-group.tf
+++ security-group.tf
@@ -26,3 +26,39 @@ resource "aws_security_group" "packy-v2-ecs-sg" {
       Name = "packy-v2-ecs-sg"
     }
 }
+
+resource "aws_security_group" "packy-v2-web-sg" {
+    vpc_id = aws_vpc.packy-v2-vpc.id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "packy-v2-web-sg"
+    }
+}
