cc1096d1d32dc2ea153294c84bd51fe6d0f2d12f
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-09-13 18:27:41 +0900

    fix: 웹 운영 서버 ECR 리소스 추가

--- ecr.tf
+++ ecr.tf
@@ -10,11 +10,16 @@ resource "aws_ecr_repository" "packy-v2-web-dev" {
   name = "packy-v2-web-dev"
 }
 
-resource "aws_ecr_lifecycle_policy" "pack_v2_lifecycle" {
+resource "aws_ecr_repository" "packy-v2-web-prod" {
+  name = "packy-v2-web-prod"
+}
+
+resource "aws_ecr_lifecycle_policy" "packy-v2-lifecycle" {
   for_each = {
     dev  = aws_ecr_repository.packy-v2-dev.name
     prod = aws_ecr_repository.packy-v2-prod.name
     web-dev = aws_ecr_repository.packy-v2-web-dev.name
+    web-prod = aws_ecr_repository.packy-v2-web-prod.name
   }
 
   repository = each.value
